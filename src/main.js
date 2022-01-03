928dbf8048dafcd401476bef2eb9bb737cef53f9
Takumi <nase49946@gmail.com>
2022-01-04 01:22:16 +0900

    検索追加

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-let apiData = null;
-
 let map;
 let marker;
 let geocoder;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: { lat: 36.0047, lng: 137.5936 },
+  });
+
+  geocoder = new google.maps.Geocoder();
+}
 
 const addMaker = (place) => {
   marker = new google.maps.Marker({
@@ -18,57 +25,54 @@ const deleteMakers = () => {
   marker = null;
 };
 
-const changeMap = (place) => {
+const changeMap = () => {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: place,
+    zoom: 6,
+    center: { lat: 36.0047, lng: 137.5936 },
   });
 };
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: { lat: 35.772296190919874, lng: 139.51982984246817 },
-  });
+document.getElementById('form').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const value = e.currentTarget.firstElementChild.value;
+  const api = await axios.get(
+    `https://peaceful-bayou-19080.herokuapp.com/api/v1/place?value=${encodeURIComponent(
+      value
+    )}`
+  );
+  const results = api.data.results;
 
-  marker = new google.maps.Marker({
-    position: { lat: 35.772296190919874, lng: 139.51982984246817 },
-    map,
-  });
+  if (!results.shop) {
+    alert('条件を絞り込んでください。');
+    return;
+  }
 
-  geocoder = new google.maps.Geocoder();
+  if (results.results_available === 0) {
+    alert('ヒットした件数は0件です。');
+    return;
+  }
 
-  document.getElementById('form').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const value = e.currentTarget.firstElementChild.value;
+  results.shop
+    .map((shop) => shop.address)
+    .forEach((address) => {
+      geocoder.geocode({ address }, (results, status) => {
+        if (status == google.maps.GeocoderStatus.OK) {
+          const bounds = new google.maps.LatLngBounds();
 
-    const api = await axios.get(
-      `https://peaceful-bayou-19080.herokuapp.com/api/v1/place?value=${encodeURIComponent(
-        value
-      )}`
-    );
+          if (results[0].geometry) {
+            let latlng = results[0].geometry.location;
 
-    if (api.data) {
-    }
-
-    console.log(api.data);
-
-    geocoder.geocode({ address: value }, (results, status) => {
-      if (status == google.maps.GeocoderStatus.OK) {
-        const bounds = new google.maps.LatLngBounds();
-
-        if (results[0].geometry) {
-          let latlng = results[0].geometry.location;
-
-          bounds.extend(latlng);
-          changeMap(latlng);
+            bounds.extend(latlng);
+            addMaker(latlng);
+          }
+        } else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+          alert('見つかりません');
+        } else {
+          console.log(status);
+          alert('文字を入力してください');
         }
-      } else if (status == google.maps.GeocoderStatus.ZERO_RESULTS) {
-        alert('見つかりません');
-      } else {
-        console.log(status);
-        alert('文字を入力してください');
-      }
+      });
     });
-  });
-}
+
+  changeMap();
+});
